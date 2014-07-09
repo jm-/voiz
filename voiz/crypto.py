cceead8eec1154cf4e61532d05a9f29b1b98052e
Julian Melchert <jm@dcd.net.nz>
2014-07-09 21:05:06 +1200

    packet sending

--- voiz/crypto.py
+++ voiz/crypto.py
@@ -5,16 +5,19 @@ Stores stateful cryptographic routines
 
 from os.path import expanduser, isfile
 from pickle import load as pickleLoad, dump as pickleDump
+from logging import getLogger
 
 from Crypto import Random
-from Crypto.Hash import SHA256
+from Crypto.Hash import SHA256, HMAC
 
-VOIZ_CACHE_PATH = expanduser('~/.voiz_cache')
+VOIZ_CACHE_PATH = '~/.voiz_cache'
 
 class VoiZCache():
 
     def __init__(self, path=VOIZ_CACHE_PATH):
-        self.path = path
+        self.logger = getLogger('cache')
+        self.path = expanduser(path)
+        self.logger.debug('Using VoiZ cache file `%s`' % self.path)
         self.load()
 
     def newZID(self):
@@ -31,19 +34,22 @@ class VoiZCache():
             self.cache = pickleLoad(fp)
 
     def save(self):
-        with open(self.path, 'wb') as fp:
+        with open(self.path, 'rb') as fp:
             pickleDump(self.cache, fp)
-
-    def getZID(self):
-        return self.cache['ZID']
 
 class VoiZMAC():
 
     def __init__(self):
+        self.logger = getLogger('mac')
         self.prng = Random.new()
+        self._computeHashChain()
 
     def _computeHashChain(self):
+        self.logger.debug('Generating MAC hash chain...')
         self.h0 = self.prng.read(32)
         self.h1 = SHA256.new(self.h0).digest()
         self.h2 = SHA256.new(self.h1).digest()
         self.h3 = SHA256.new(self.h2).digest()
+
+    def hmac_h2(self, payload):
+        return HMAC.new(self.h2, payload, SHA256).digest()
--- voiz/protocol.py
+++ voiz/protocol.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from logging import getLogger
+from struct import Struct
+
 PKT_HELLO       = 0x00
 PKT_HELLOACK    = 0x01
 PKT_COMMIT      = 0x02
@@ -7,3 +10,17 @@ PKT_DHPART1     = 0x03
 PKT_DHPART2     = 0x04
 PKT_CONFIRM1    = 0x05
 PKT_CONFIRM2    = 0x06
+
+class VoiZPacketFactory():
+
+    def __init__(self, cache, mac):
+        self.logger = getLogger('pkt-factory')
+        self.cache = cache
+        self.mac = mac
+
+    def gen_pkt_payload_hello(self):
+        return chr(PKT_HELLO) + self.mac.h3 + self.cache.getZID()
+
+    def gen_pkt_hello(self):
+        payload = self.gen_pkt_payload_hello()
+        return payload + self.mac.hmac_h2(payload)
